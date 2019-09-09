39aba6821e87b307b2ae810dc12305953a5c549f
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-09 11:28:12 -0300

    Validating erros on user signup

--- app/controllers/cadastroController.js
+++ app/controllers/cadastroController.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(req, res) {
-    res.render('cadastro');
+    res.render('cadastro', { validacao: {}, formData: {} });
+}
+
+module.exports.cadastrar = function(req, res) {
+
+    var formData = req.body;
+
+    req.assert('nome','Nome é obrigatório').notEmpty()
+    req.assert('usuario','Usuário é obrigatório').notEmpty()
+    req.assert('senha','Senha é obrigatório').notEmpty()
+    req.assert('casa','Casa é obrigatório').notEmpty()
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('cadastro', { validacao: errors, formData: formData });
+        return;
+    }
+
+    res.render('cadastro', { validacao: errors, formData: {} });
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -2,4 +2,8 @@ module.exports = function(application) {
     application.get('/cadastro', function(req, res) {
         application.controllers.cadastroController.cadastro(req, res);
     });
+
+    application.post('/cadastrar', function(req, res) {
+        application.controllers.cadastroController.cadastrar(req, res);
+    })
 }
